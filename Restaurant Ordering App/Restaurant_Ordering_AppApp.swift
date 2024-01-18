60cdaa1c35b1187156f4e920b82061ec2ae2dde6
Sashu-Ponnaganti <88560514+Sashu-Ponnaganti@users.noreply.github.com>
2024-01-17 21:29:14 -0500

    Initial Commit

---
+++ Restaurant Ordering App/Restaurant_Ordering_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_Ordering_AppApp.swift
+//  Restaurant Ordering App
+//
+//  Created by Sashu Ponnaganti on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_Ordering_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
